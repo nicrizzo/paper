2437715b1e41cfc85f7247f7c4495b9caea2b5ee
Nicola Rizzo <nicola.rizzo@gmail.com>
2013-04-01 21:49:10 +0200

    Firefox fix

--- src/modules/interpolators/quadratic.js
+++ src/modules/interpolators/quadratic.js
@@ -6,8 +6,8 @@ define(function(){
 				context.fillRect(stroke[0].x, stroke[0].y, 1, 1);
 				return;
 			}
-			context.moveTo(stroke[0].x, stroke[0].y);
 			context.beginPath();
+			context.moveTo(stroke[0].x, stroke[0].y);
 			for(var ii = 0, ll = stroke.length-2; ii < ll; ii++){
 				context.quadraticCurveTo(x = (point = stroke[ii]).x, y = point.y, (x + (nx = (nextPoint = stroke[ii + 1]).x)) >> 1, (y + (ny = nextPoint.y)) >> 1);
 			}
